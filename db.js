2f1d80f73d7de3607dbbe3049e589311aaa86264
Developer <dev@example.com>
2026-01-12 10:37:14 +0530

    new check

--- db.js
+++ db.js
@@ -1,20 +1,27 @@
 const mongoose = require("mongoose");
 
-let isConnected = false;
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  if (isConnected) return;
+  if (cached.conn) return cached.conn;
 
   if (!process.env.MONGODB_URI) {
     throw new Error("❌ MONGODB_URI not defined");
   }
 
-  await mongoose.connect(process.env.MONGODB_URI, {
-    bufferCommands: false,
-  });
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 30000,
+    });
+  }
 
-  isConnected = true;
+  cached.conn = await cached.promise;
   console.log("✅ MongoDB connected");
+  return cached.conn;
 };
 
 module.exports = connectDB;
